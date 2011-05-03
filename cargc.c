f6e31c90773521f5deda17182c88a71c4dbeb9ad
Callum Rogers <callumrogers@hotmail.co.uk>
2011-05-03 01:51:10 +0100

    Playing around with registers and inline asm

--- cargc.c
+++ cargc.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+int *registers;
+
 int main()
 {
+	registers = malloc(4*sizeof(int));
+
+	print_regs();
+	read_registers();
+	print_regs();
+
 	return 0;
+}
+
+void print_regs()
+{
+	printf("%d, %d, %d, %d\n", registers[0], registers[1], registers[2], registers[3]);
 }
 
 int GCalloc(int size)
@@ -13,4 +26,20 @@ int GCalloc(int size)
 int GCfree(int *ptr)
 {
 	
+}
+
+void read_registers(void)
+{
+	int r;
+	asm ("movl %%eax, %0\n" :"=r"(r));
+	registers[0] = r;
+	
+	asm ("movl %%ebx, %0\n" :"=r"(r));
+	registers[1] = r;
+	
+	asm ("movl %%ecx, %0\n" :"=r"(r));
+	registers[2] = r;
+	
+	asm ("movl %%edx, %0\n" :"=r"(r));
+	registers[3] = r;
 }
